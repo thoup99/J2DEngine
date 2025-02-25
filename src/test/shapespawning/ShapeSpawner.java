471820adb67a7c20e4d114b06899d8771e4e83b1
thoup99 <thoup99@outlook.com>
2025-02-25 16:43:34 -0500

    Updated Shape Spawning Example

--- src/test/shapespawning/ShapeSpawner.java
+++ src/test/shapespawning/ShapeSpawner.java
@@ -2,24 +2,37 @@ package test.shapespawning;
 
 import j2d.attributes.position.Position2D;
 import j2d.components.graphics.shapes.*;
+import j2d.components.graphics.shapes.Shape;
 import j2d.engine.GameObject;
 import j2d.engine.input.keyboard.KeyHandler;
 import j2d.engine.input.keyboard.KeySubscriber;
 import j2d.engine.render.Renderer;
 import j2d.engine.window.Window;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ShapeSpawner extends GameObject implements KeySubscriber {
     List<Shape> shapes;
+    Random random;
 
     ShapeSpawner() {
         shapes = new ArrayList<>();
+        random = new Random();
 
         int[] keys = {KeyEvent.VK_S, KeyEvent.VK_C, KeyEvent.VK_R, KeyEvent.VK_P};
         KeyHandler.subscribe(this, keys);
+    }
+
+    private Color getRandomColor() {
+        return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+    }
+
+    public int getRandomInt(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
     }
 
     @Override
@@ -34,15 +47,23 @@ public class ShapeSpawner extends GameObject implements KeySubscriber {
 
     @Override
     public void keyPressed(int key) {
-        Position2D mousePos = Window.getMousePosition();
         if (key == KeyEvent.VK_S) {
+            Position2D mousePos = Window.getMousePosition();
+            int size = getRandomInt(10, 20);
+
             Square newSquare = new FillSquare(this, 0,
-                    new Position2D(mousePos.getX() - 10, mousePos.getY() - 10),
-                    new Position2D(mousePos.getX() + 10, mousePos.getY() + 10));
+                    new Position2D(mousePos.getX() - size, mousePos.getY() - size),
+                    new Position2D(mousePos.getX() + size, mousePos.getY() + size));
+
+            newSquare.setColor(getRandomColor());
             shapes.add(newSquare);
         }
         if (key == KeyEvent.VK_C) {
-            Circle newCircle = new FillCircle(this, 0, mousePos, 10);
+            Position2D mousePos = Window.getMousePosition();
+
+            Circle newCircle = new FillCircle(this, 0, mousePos, getRandomInt(10, 30));
+
+            newCircle.setColor(getRandomColor());
             shapes.add(newCircle);
         }
 
